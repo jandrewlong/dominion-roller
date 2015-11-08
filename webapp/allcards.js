4bddebd49d003924294533259c797027b6d06e76
J Andrew Long <jandrewlong@gmail.com>
2015-11-07 21:41:01 -0500

    added, updated Adventures images

--- webapp/allcards.js
+++ webapp/allcards.js
@@ -278,6 +278,6 @@ allcards_data=
 {"id":"hinterlands","name":"Hinterlands","image":"299px-Hinterlands.jpg","icon_image":"20px-Hinterlands_icon.png"},
 {"id":"darkages","name":"Dark Ages","image":"300px-Dark_Ages.jpg","icon_image":"17px-Dark_Ages_icon.png"},
 {"id":"guilds","name":"Guilds","image":"Guilds.jpg","icon_image":"17px-Guilds_icon.png"}
-{"id":"adventures","name":"Adventures","image":"Adventures.jpg","icon_image":"17px-Adventures_icon.png"}
+{"id":"adventures","name":"Adventures","image":"Adventures.jpg","icon_image":"20px-adventures_icon.png"}
 ]
 }
